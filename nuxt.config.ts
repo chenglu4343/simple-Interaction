d75a36c71a3ee78cdf8a487850e2e50e4f99b2e5
chenglu <chenglu4343@qq.com>
2023-05-17 15:58:56 +0800

    🦄 refactor: move css to assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   ],
   css: [
     'normalize.css',
-    '~/css/scrollbar.css',
-    '~/css/global.css',
+    '~/assets/css/scrollbar.css',
+    '~/assets/css/global.css',
   ],
 })

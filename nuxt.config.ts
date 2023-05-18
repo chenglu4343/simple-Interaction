74e831242b6a073f05a9039bde9ef7ad9b357721
chenglu <chenglu4343@qq.com>
2023-05-18 10:15:29 +0800

    🐳 chore: add simple-global-scrollbar.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   ],
   css: [
     'normalize.css',
-    '~/assets/css/scrollbar.css',
+    'simple-global-scrollbar.css',
     '~/assets/css/global.css',
   ],
 })

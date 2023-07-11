16b7fc1bce055fa29d5e433845c5292118ed4055
chenglu <chenglu4343@qq.com>
2023-07-11 02:46:54 +0000

    ✨ feat: add options input

--- pages/hoverPreviewImage/index.vue
+++ pages/hoverPreviewImage/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { NInputNumber } from 'naive-ui'
 import { useHoverPreviewImg } from './helpers/useHoverPreviewImg'
 import defaultImgSrc from '~/assets/image/pull-shark-bronze.png'
 
@@ -7,10 +8,20 @@ useHead({
 })
 
 const containerRef = ref<HTMLElement | undefined>()
+const containerWidth = ref(200)
+const containerHeight = ref(200)
+const hoverWidth = ref(100)
+const hoverHeight = ref(100)
+const previewWidth = ref(300)
 
 const { containerStyle, hoverBoxStyle, previewBoxStyle } = useHoverPreviewImg({
   imgSrc: defaultImgSrc,
   containerRef,
+  containerWidth,
+  containerHeight,
+  hoverWidth,
+  hoverHeight,
+  previewWidth,
 })
 </script>
 
@@ -31,6 +42,30 @@ const { containerStyle, hoverBoxStyle, previewBoxStyle } = useHoverPreviewImg({
     <div
       class="preview-box" :style="previewBoxStyle"
     />
+
+    <!-- input -->
+    <div class="mt-4">
+      <div class="row">
+        <span>containerWidth:</span>
+        <NInputNumber v-model:value="containerWidth" />
+      </div>
+      <div class="row">
+        <span>containerHeight:</span>
+        <NInputNumber v-model:value="containerHeight" />
+      </div>
+      <div class="row">
+        <span>hoverWidth:</span>
+        <NInputNumber v-model:value="hoverWidth" />
+      </div>
+      <div class="row">
+        <span>hoverHeight:</span>
+        <NInputNumber v-model:value="hoverHeight" />
+      </div>
+      <div class="row">
+        <span>previewWidth:</span>
+        <NInputNumber v-model:value="previewWidth" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -54,5 +89,13 @@ const { containerStyle, hoverBoxStyle, previewBoxStyle } = useHoverPreviewImg({
 
 .preview-box{
     margin: auto;
+}
+
+.row{
+  display: flex;
+
+  &>span{
+    min-width: 150px;
+  }
 }
 </style>

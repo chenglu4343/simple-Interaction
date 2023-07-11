14edac3770795629ea07dfe5b1640eeca580ea33
chenglu <chenglu4343@qq.com>
2023-07-11 02:29:32 +0000

    🦄 refactor: refactor in composables

---
+++ pages/hoverPreviewImage/helpers/useHoverPreviewImg.ts
@@ -0,0 +1,170 @@
+import { type MaybeRefOrGetter, toValue } from '@vueuse/core'
+import type { CSSProperties } from 'nuxt/dist/app/compat/capi'
+
+export interface OPTIONS {
+  imgSrc: MaybeRefOrGetter<string>
+
+  /**
+   * container
+   * There should be only one img and a hover box in the container
+   *
+   * and img style {
+   *    width: 100%;
+   *    height: 100%;
+   * }
+   */
+  /** container element */
+  containerRef: MaybeRefOrGetter<HTMLElement | undefined>
+  /**
+   * containerWidth
+   * @default 200
+   */
+  containerWidth?: MaybeRefOrGetter<Number>
+  /**
+   * containerHeight
+   * @default 200
+   */
+  containerHeight?: MaybeRefOrGetter<Number>
+
+  // hover
+  /**
+   * hoverWidth
+   * @default 100
+   */
+  hoverWidth?: MaybeRefOrGetter<Number>
+  /**
+   * hoverWidth
+   * @default 100
+   */
+  hoverHeight?: MaybeRefOrGetter<Number>
+  /**
+   * hoverBg
+   * @default "rgba(24, 144, 255,.6)"
+   */
+  hoverBg?: MaybeRefOrGetter<String>
+
+  // preview
+  /**
+   * previewWidth
+   * @default 300
+   */
+  previewWidth?: MaybeRefOrGetter<Number>
+}
+
+export function useHoverPreviewImg(options: OPTIONS) {
+  const {
+    imgSrc,
+    containerRef,
+    containerWidth = 200,
+    containerHeight = 200,
+    hoverWidth = 100,
+    hoverHeight = 100,
+    previewWidth = 300,
+    hoverBg = 'rgba(24, 144, 255,.6)',
+  } = options
+
+  const { x, y } = useMouse({
+    target: containerRef,
+  })
+
+  const mousePosition = computed(() => {
+    let minusLeft = 0
+    let minusTop = 0
+    let containerWidth = Infinity
+    let containerHeight = Infinity
+    const containerElement = toValue(containerRef)
+
+    if (containerElement) {
+      const { pageLeft, pageTop } = getPageLeftAndTop(containerElement)
+      minusLeft = pageLeft
+      minusTop = pageTop
+      containerWidth = containerElement.offsetWidth
+      containerHeight = containerElement.offsetHeight
+    }
+
+    return {
+      mouseX: Math.min(Math.max(toValue(hoverWidth as any) / 2, x.value - minusLeft), containerWidth - toValue(hoverWidth as any) / 2),
+      mouseY: Math.min(Math.max(toValue(hoverHeight as any) / 2, y.value - minusTop), containerHeight - toValue(hoverHeight as any) / 2),
+    }
+  })
+
+  const aspectRatio = computed(() => {
+    return toValue(hoverWidth as any) / toValue(hoverHeight as any)
+  })
+
+  const containerStyle = computed<CSSProperties>(() => {
+    return {
+      width: `${toValue(containerWidth)}px`,
+      height: `${toValue(containerHeight)}px`,
+      cursor: 'move',
+      position: 'relative',
+    }
+  })
+
+  const hoverBoxStyle = computed<CSSProperties>(() => {
+    return {
+      width: `${toValue(hoverWidth)}px`,
+      aspectRatio: aspectRatio.value,
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      backgroundColor: toValue(hoverBg as any),
+      transform: `translateX(-50%)
+                    translateY(-50%)
+                    translateX(${mousePosition.value.mouseX}px)
+                    translateY(${mousePosition.value.mouseY}px)`,
+    }
+  })
+
+  const previewBoxStyle = computed<CSSProperties>(() => {
+    const previewHeight = toValue(previewWidth as any) / aspectRatio.value
+    /** @default 600 */
+    const xBgSize = toValue(containerWidth as any) / toValue(hoverWidth as any) * toValue(previewWidth as any)
+    /** @default 600 */
+    const yBgSize = toValue(containerHeight as any) / toValue(hoverHeight as any) * toValue(previewHeight as any)
+
+    /** @default -3 */
+    const xMoveStep = -xBgSize / toValue(containerWidth as any)
+    /** @default -3 */
+    const yMoveStep = -yBgSize / toValue(containerHeight as any)
+
+    const xPosition = (mousePosition.value.mouseX - toValue(hoverWidth as any) / 2) * xMoveStep
+    const yPosition = (mousePosition.value.mouseY - toValue(hoverHeight as any) / 2) * yMoveStep
+
+    return {
+      width: `${toValue(previewWidth)}px`,
+      aspectRatio: aspectRatio.value,
+      backgroundImage: `url(${imgSrc})`,
+      overflow: 'hidden',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: `${xBgSize}px ${yBgSize}px`,
+      backgroundPosition: `left ${xPosition}px top ${yPosition}px`,
+    }
+  })
+
+  return {
+    containerStyle,
+    hoverBoxStyle,
+    previewBoxStyle,
+  }
+}
+
+/**
+ * get offsetTop and offsetLeft to window
+ */
+function getPageLeftAndTop(element: HTMLElement) {
+  let pageLeft = element.offsetLeft
+  let pageTop = element.offsetTop
+  let parent = element.offsetParent as HTMLElement
+
+  while (parent) {
+    pageLeft += parent.offsetLeft
+    pageTop += parent.offsetTop
+    parent = parent.offsetParent as HTMLElement
+  }
+
+  return {
+    pageLeft,
+    pageTop,
+  }
+}
--- pages/hoverPreviewImage/index.vue
+++ pages/hoverPreviewImage/index.vue
@@ -1,95 +1,41 @@
 <script setup lang="ts">
+import { useHoverPreviewImg } from './helpers/useHoverPreviewImg'
 import defaultImgSrc from '~/assets/image/pull-shark-bronze.png'
 
 useHead({
   title: 'hoverPreviewImage',
 })
 
-const HOVER_W = 100
-const HOVER_H = 100
-const HOVER_RATIO = HOVER_W / HOVER_H
-const PREVIEW_W = 300
 const containerRef = ref<HTMLElement | undefined>()
-const { x: mouseX, y: mouseY } = useMouse({
-  target: containerRef,
+
+const { containerStyle, hoverBoxStyle, previewBoxStyle } = useHoverPreviewImg({
+  imgSrc: defaultImgSrc,
+  containerRef,
 })
-const styleVars = computed(() => {
-  let minusLeft = 0
-  let minusTop = 0
-  let containerWidth = Infinity
-  let containerHeight = Infinity
-
-  if (containerRef.value) {
-    const { pageLeft, pageTop } = getPageLeftAndTop(containerRef.value)
-    minusLeft = pageLeft
-    minusTop = pageTop
-    containerWidth = containerRef.value.offsetWidth
-    containerHeight = containerRef.value.offsetHeight
-  }
-
-  return {
-    '--hover-w': `${HOVER_W}px`,
-    '--hover-h': `${HOVER_H}px`,
-    '--hover-ratio': HOVER_RATIO,
-    '--preview-w': `${PREVIEW_W}px`,
-    '--mouse-x': Math.min(Math.max(HOVER_W / 2, mouseX.value - minusLeft), containerWidth - HOVER_W / 2),
-    '--mouse-y': Math.min(Math.max(HOVER_H / 2, mouseY.value - minusTop), containerHeight - HOVER_H / 2),
-  }
-})
-
-/**
- * get offsetTop and offsetLeft to window
- */
-function getPageLeftAndTop(element: HTMLElement) {
-  let pageLeft = element.offsetLeft
-  let pageTop = element.offsetTop
-  let parent = element.offsetParent as HTMLElement
-
-  while (parent) {
-    pageLeft += parent.offsetLeft
-    pageTop += parent.offsetTop
-    parent = parent.offsetParent as HTMLElement
-  }
-
-  return {
-    pageLeft,
-    pageTop,
-  }
-}
 </script>
 
 <template>
-  <div class="flex flex-col items-center" :style="styleVars">
+  <div class="flex flex-col items-center">
     <div
       ref="containerRef"
       class="container"
+      :style="containerStyle"
     >
       <!-- img-box -->
       <img :src="defaultImgSrc">
       <!-- hover-box -->
-      <div class="hover-box" />
+      <div class="hover-box" :style="hoverBoxStyle" />
     </div>
 
     <!-- preview-box -->
     <div
-      class="preview-box" :style="{
-        '--bg-url': `url(${defaultImgSrc})`,
-      }"
+      class="preview-box" :style="previewBoxStyle"
     />
   </div>
 </template>
 
 <style lang="scss">
 .container{
-  --container-width: 200px;
-  --container-height:200px;
-  --hover-bg: rgba(24, 144, 255,.6);
-
-  position: relative;
-  width: var(--container-width);
-  height: var(--container-height);
-  overflow: hidden;
-  cursor: move;
   margin: auto;
 
   img{
@@ -99,18 +45,6 @@ function getPageLeftAndTop(element: HTMLElement) {
 
   .hover-box{
     display: none;
-    position: absolute;
-    width: var(--hover-w);
-    aspect-ratio: var(--hover-ratio);
-    background-color: var(--hover-bg);
-    box-sizing: border-box;
-    left: 0;
-    top: 0;
-
-    transform: translateX(-50%)
-                translateY(-50%)
-                translateX(calc(var(--mouse-x) * 1px))
-                translateY(calc(var(--mouse-y) * 1px));
   }
 
   &:hover .hover-box{
@@ -119,13 +53,6 @@ function getPageLeftAndTop(element: HTMLElement) {
 }
 
 .preview-box{
-    width: var(--preview-w);
-    aspect-ratio: var(--hover-ratio);
     margin: auto;
-    overflow: hidden;
-    background-image: var(--bg-url);
-    background-size: calc(200px / 100px * 300px);
-    background-position: left calc((var(--mouse-x) - 50) * -3px) top calc((var(--mouse-y) - 50) * -3px);
-    background-repeat: no-repeat;
 }
 </style>

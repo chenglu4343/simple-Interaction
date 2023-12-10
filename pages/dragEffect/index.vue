6ce3738d72c3d97f002ed1cccd80495138e4ab8d
chenglu <chenglu4343@qq.com>
2023-12-10 21:55:03 +0800

    refactor: adjust structure

--- pages/dragEffect/index.vue
+++ pages/dragEffect/index.vue
@@ -3,8 +3,10 @@ import { DragList, ProgressBar } from './components'
 </script>
 
 <template>
-  <h1>drag List</h1>
-  <DragList />
-  <h1>progress Bar</h1>
-  <ProgressBar />
+  <main>
+    <h1>drag List</h1>
+    <DragList />
+    <h1>progress Bar</h1>
+    <ProgressBar />
+  </main>
 </template>
